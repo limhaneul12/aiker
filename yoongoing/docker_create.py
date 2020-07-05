b252fab510b752c9b87af95f2c70ec7f1df6fc6c
임하늘 <ssky1267@naver.com>
2020-07-05 18:51:09 +0900

    일단 수정해봄;;;

--- yoongoing/docker_create.py
+++ yoongoing/docker_create.py
@@ -16,22 +16,42 @@ def create_docker():
     '''
 
     #docker remote API와 통신할 길 뚫는 작업
-    client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    print(client.version())
+    client = docker.APIClient(base_url='http://127.0.1.1:4243')
+    """
+    # docker hub에서 이미지 다운 여기서 먼저 이미지 다운 받을 것
+    for line in client.pull("busybox", stream=True):
+        print(json.dumps(json.loads(line), indent=4))
+    """
+    # container 생성
+    container = client.create_container(image="busybox:latest", command="/bin/sleep 30")
+    # 생성된 container inspect
+    """
+     curl -L http://127.0.1.1:4243/containers/json?all=1      
+     
+    [{"Id":"ab38a05f8cea8bd853bd132353064fcc70dad1881a971ba45d84704286c836fd",
+    "Names":["/silly_tharp"],
+    "Image":"busybox:latest",
+    "ImageID":"sha256:c7c37e472d31c1685b48f7004fd6a64361c95965587a951692c5f298c6685998",
+    "Command":"/bin/sleep 30","Created":1593941327,
+    "Ports":[],
+    "Labels":{},
+    "State":"created",
+    "Status":"Created",
+    "HostConfig":{"NetworkMode":"default"},
+    "NetworkSettings":{"Networks":{"bridge":{"IPAMConfig":null,"Links":null,"Aliases":null,
+                        "NetworkID":"","EndpointID":"","Gateway":"","IPAddress":"","IPPrefixLen":0,"IPv6Gateway":"",
+                        "GlobalIPv6Address":"","GlobalIPv6PrefixLen":0,"MacAddress":"","DriverOpts":null}}},"Mounts":[]}]
 
-    #원래는 웹서버로 부터 받아온 인자값으로 넣어야 하지만, 지금은 걍 예시로 넣는 중
-    container = client.create_container(
-        'busybox', # !!!!!!!!!!!여기에 에러남 왠지 몰겠음!!!!!!!!!!!
-        'ls',
-        ports=[1111, 2222],
-        host_config=client.create_host_config(port_bindings={1111: 4567, 2222: None}))
+    """
+    container_inspect = client.containers(latest=True)
 
-    print(container)  # 원래대로라면 도커의 정보가 잘~ 나와야함
+    Id = container_inspect[0]["Id"]
+    Name = container_inspect[0]["Names"]
+    Image = container_inspect[0]["Image"]
+    port = container_inspect[0]["Ports"]
+    Command = container_inspect[0]["Command"]
+    test_info = [Id, Name, Image, port, Command]
 
-    #컨테이너를 create_container로 만들면 start를 해줘야 한다고함
-    response = client.start(container=container.get('Id'))
-    print(response)  # 음 모르겠음
-
-
+    return test_info
 if __name__ == "__main__":
     print(create_docker())
