048c752818721621a1413112832b92beab8cbc3b
임하늘 <sky81219>
2020-08-20 23:46:33 +0900

    -

--- yoongoing/test_client.py
+++ yoongoing/test_client.py
@@ -7,7 +7,7 @@ create(POST), list(GET), delete(DELETE) 요청 테스트하는 파이썬코드
 import requests
 import urllib.parse
 
-url = 'http://0.0.0.0:8000/create_docker'
+url = 'http://192.168.0.23:8000/create_docker'
 
 #도커 생성
 
@@ -29,22 +29,7 @@ while True:
         continue
 
 
-url = 'http://192.168.0.23:8000/create_docker'
 
-#도커 생성
-while True:
-    params = {'name':input("name >"), 'image':input("image >"), 
-              'port':'8000', 'command':'test/image2',
-              'label_idx':1, 'user_idx':1}
-    res = requests.post(url=url, params=params)
-    print(res.text)
-
-    continue_ = input("continue? > ")
-
-    if continue_ in ["No", "n", "no", "exit", "false"]:
-        break
-    elif continue_ in ["yes", "y"]:
-        continue
 
 # url = 'http://localhost:8000/list'
 # #도커 조회
